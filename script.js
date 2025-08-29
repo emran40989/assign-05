0f3ff5b626374e0384998fd07970ef56a90f09e9
emran40989 <emran40989@gmail.com>
2025-08-29 19:15:20 +0600

    js added

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
+const hearts = document.getElementsByClassName("heart-icon-count");
+const heartCounter = document.getElementById("heart-count")
 
-document.getElementById
+for (let i = 0; i < hearts.length; i++) {
+    hearts[i].addEventListener("click", function() {
+        const defaultHeart = parseInt(heartCounter.innerText);
+        const newHeart = defaultHeart + 1;
+        heartCounter.innerText = newHeart;
+    });
+}
+
+const copied = document.getElementsByClassName("copy-button");
+const copyCounter = document.getElementById("copy-count") 
+
+for (let i = 0; i < copied.length; i++) {
+    copied[i].addEventListener("click", function() {
+        const defaultCopy = parseInt(copyCounter.innerText);
+        const newCopy = defaultCopy + 1;
+        copyCounter.innerText = newCopy;
+    });
+}
+
+const called = document.getElementsByClassName("call-button");
+
